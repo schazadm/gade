c69a94df5c7e91ef0d260aede1f71fea45ce3e18
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-04-19 11:37:07 +0200

    cyber space part 04

---
+++ mini-project_cyberspace-olympics/HorseJump/Assets/donut/Spin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Rotate(10f * Time.deltaTime, 30f * Time.deltaTime, 20f * Time.deltaTime, Space.Self);
+    }
+}
