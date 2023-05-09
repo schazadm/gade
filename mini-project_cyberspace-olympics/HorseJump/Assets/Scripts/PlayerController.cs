981702b88c1317ecd742739117211ae7c66d9d9c
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-05-09 11:41:18 +0200

    did fifth assignment

--- mini-project_cyberspace-olympics/HorseJump/Assets/Scripts/PlayerController.cs
+++ mini-project_cyberspace-olympics/HorseJump/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-    public Rigidbody2D rb;
+    private float moveSpeed = 10f;
+    private Rigidbody2D rb;
     private float moveX;
+    private Animator animator;
+    public ParticleSystem dust;
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +26,27 @@ public class PlayerController : MonoBehaviour
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
+
+        if (rb.velocity.y >= 0f)
+        {
+            animator.SetBool("isInAir", true);
+        }
+        else
+        {
+            animator.SetBool("isInAir", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "donut")
+        {
+            other.gameObject.GetComponent<AudioSource>().Play();
+        }
+
+        if (other.gameObject.tag == "platform")
+        {
+            dust.Play();
+        }
     }
 }
